8c4ef607c93789217f83c824cd7ca87dc8ab88bc
sebas <s.q.wieser@gmail.com>
2024-06-02 14:50:05 +0200

    1st summary

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ from transformers import AutoTokenizer, TFAutoModel
 
 tokenizer = AutoTokenizer.from_pretrained("nlpaueb/legal-bert-base-uncased")
 model = TFAutoModel.from_pretrained("nlpaueb/legal-bert-base-uncased")
+
+print(dir(model))
